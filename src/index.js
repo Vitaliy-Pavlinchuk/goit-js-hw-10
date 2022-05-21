f51a9dfa20c396bc5053c147441c5b03c4f923cb
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-05-21 12:19:24 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,63 +1,25 @@
+import { fetchCountries } from './js/fetchCountries';
+import { renderCountries } from './js/renderCountries';
+import { cleanupRender } from './js/cleanupRender';
+import { refs } from './js/refs';
+
 import './css/styles.css';
-import fetchCountries from './fetchCountries'
 import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
+refs.inputSearch.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
+function onInput(e) {
+  const name = e.target.value.trim();
 
-
-
-
-const input = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-
-input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
-
-function onInputChange() {
-  const isFilled = input.value.trim();
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
-  if (isFilled) {
-    fetchCountries(isFilled)
-      .then(dataProcessing)
-      .catch(error => {
-        Notify.failure('Oops, there is no country with that name');
-        console.log(error);
-      });
+  if (!name) {
+    cleanupRender();
+    return;
   }
 
-  function dataProcessing(data) {
-    if (data.length > 10) {
-      Notify.info('Too many matches found. Please enter a more specific name.');
-
-      return;
-    }
-
-    markup(data);
-  }
-
-  function markup(data) {
-    const markupData = data
-      .map(({ flags: { svg }, name: { official } }) => {
-        return `<li><img src="${svg}" alt="${official}" width="100" height="50"/>${official}</li>`;
-      })
-      .join('');
-
-    if (data.length === 1) {
-      const languages = Object.values(data[0].languages).join(', ');
-
-      const markupInfo = `<ul>
-      <li>Capital: ${data[0].capital}</li>
-      <li>Population: ${data[0].population}</li>
-      <li>Languages: ${languages}</li>
-      </ul>`;
-
-      countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
-    }
-    return countryList.insertAdjacentHTML('afterbegin', markupData);
-  }
+  fetchCountries(name)
+    .then(renderCountries)
+    .catch(() => Notify.failure('Oops, there is no country with that name'));
 }
